9687197f85979bd3215da1032b8f0194be5e6d73
Caskman <eric.cascketta@gmail.com>
2013-01-19 13:04:11 -0700

    updated version number

--- CaskChat/src/Parameters.java
+++ CaskChat/src/Parameters.java
@@ -8,9 +8,9 @@ import javax.imageio.ImageIO;
 public class Parameters {
 	
 	public static final int port = 2126;
-	public static final String SERVER = "localhost"; // apartment is 70.176.84.115
+	public static final String SERVER = "70.176.84.115"; // apartment is 70.176.84.115
 	public static final String PASSWORD = "6972126";
-	public static final double VERSION_ID = 1.160;
+	public static final double VERSION_ID = 1.200;
 	public static BufferedImage APP_ICON;
 	public static final String rootDir = System.getProperty("user.home") + File.separator + "CaskChat" + File.separator;
 	
